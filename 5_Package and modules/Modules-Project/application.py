2cb97f2d54a0330d438d549b4a8176c88a2ad137
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-08-20 18:23:41 +0530

    Add files via upload

---
+++ 5_Package and modules/Modules-Project/application.py
@@ -0,0 +1,22 @@
+from datastructure.mylists import StudentList
+from datastructure.mylists import MyList
+from Analysis.research import MyAnalysis
+print("hello")
+
+new_list = StudentList()
+new_list.add(1)
+new_list.add(2)
+new_list.add(3)
+new_list.print()
+
+new_list = MyList()
+new_list.add(1)
+new_list.add(2)
+new_list.add(3)
+new_list.print()
+
+new_analysis = MyAnalysis()
+new_analysis.add(1)
+new_analysis.add(2)
+new_analysis.add(3)
+new_analysis.print()
