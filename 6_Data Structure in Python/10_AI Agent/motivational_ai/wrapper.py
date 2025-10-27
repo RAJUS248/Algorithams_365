e556e9909645719357fd4e63b2eb36010f50e04e
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-10-27 11:29:26 +0530

    Delete 6_Data Structure in Python/10_AI Agent directory

--- 6_Data Structure in Python/10_AI Agent/motivational_ai/wrapper.py
+++
@@ -1,8 +0,0 @@
-from agent import MotivationalAgent
-
-class AgentWrapper:
-    def __init__(self, api_key):
-        self.agent = MotivationalAgent(api_key=api_key)
-
-    def get_motivation(self, user_name, motivation_type):
-        return self.agent.generate_response(user_name, motivation_type)
